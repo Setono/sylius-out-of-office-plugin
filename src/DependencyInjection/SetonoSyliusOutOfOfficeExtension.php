31477fde1418d23afabb2db98122cdaf349dd9cb
Joachim Løvgaard <joachim@loevgaard.dk>
2026-06-17 13:08:45 +0200

    Rename example scaffold to Setono Sylius Out of Office plugin

--- src/DependencyInjection/SetonoSyliusOutOfOfficeExtension.php
+++ src/DependencyInjection/SetonoSyliusOutOfOfficeExtension.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace Setono\SyliusOutOfOfficePlugin\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class AcmeSyliusExampleExtension extends Extension
+final class SetonoSyliusOutOfOfficeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -21,7 +21,7 @@ final class AcmeSyliusExampleExtension extends Extension
         $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $container->setParameter('acme_sylius_example.option', $config['option']);
+        $container->setParameter('setono_sylius_out_of_office.option', $config['option']);
 
         $loader->load('services.xml');
     }
